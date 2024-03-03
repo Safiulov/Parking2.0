701677fc693b412620539d56332e5a58f305fded
Safiulov <82534715+Safiulov@users.noreply.github.com>
2024-03-03 13:48:35 +0400

    ERRORS

---
+++ Empty_value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTREALISE.Errors
+{
+    internal class Empty_value: Exception
+    {
+        public Empty_value(string message):base(message) { }
+    }
+}
---
+++ Wrong_place.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTREALISE.Errors
+{
+    internal class Wrong_place: Exception
+    {
+        public Wrong_place(string message) : base(message) { }
+    }
+}
